588ad389c21933ed3ee16c3d31bc5c19172358b7
Tina Yi Jin Hsieh <hsiehyijin@gmail.com>
2025-07-18 12:51:01 -0400

    Resubmit models and Streamlit deploy script_12

--- Final_smfm_deploy.py
+++ Final_smfm_deploy.py
@@ -29,8 +29,8 @@ with col1:
     st.caption("Trained on 259 cases with cystic hygroma (CH)")
 
     age_1st = st.number_input("Maternal Age", min_value=15, max_value=50, value=32, key="age1")
-    nt_1st = st.number_input("NT (mm)", min_value=0.1, max_value=10.0, value=2.5, key="nt1")
-    crl_1st = st.number_input("CRL (mm)", min_value=5.0, max_value=100.0, value=50.0, key="crl1")
+    nt_1st = st.number_input("NT (mm)", min_value=0.1, max_value=10.0, value=2.5, step=0.1, key="nt1")
+    crl_1st = st.number_input("CRL (mm)", min_value=5.0, max_value=100.0, value=50.0, step=0.1, key="crl1")
     nipt_1st = st.selectbox("NIPT result:", ["Normal", "Abnormal", "Not Reported"], key="nipt1")
 
     st.markdown("<div style='background-color: #e0f7fa; padding: 10px; border-radius: 5px;'><b style='color: black;'>Select 1st trimester outcome:</b></div>", unsafe_allow_html=True)
@@ -97,8 +97,8 @@ with col2:
 )
     st.caption("Trained on 66 non-aneuploidy cases with CH in second trimester")
 
-    nt_2nd = st.number_input("NT (mm, T2)", min_value=0.1, max_value=10.0, value=2.5, key="nt2")
-    crl_2nd = st.number_input("CRL (mm, T2)", min_value=5.0, max_value=100.0, value=50.0, key="crl2")
+    nt_2nd = st.number_input("NT (mm, T2)", min_value=0.1, max_value=10.0, value=2.5, step=0.1, key="nt2")
+    crl_2nd = st.number_input("CRL (mm, T2)", min_value=5.0, max_value=100.0, value=50.0, step=0.1, key="crl2")
     resolved_ch = st.selectbox("Resolved CH?", ["Yes", "No"], key="resolvedCH")
 
     st.markdown("<div style='background-color: #fff1e2; padding: 10px; border-radius: 5px;'><b style='color: black;'>Select 2nd trimester outcome:</b></div>", unsafe_allow_html=True)
